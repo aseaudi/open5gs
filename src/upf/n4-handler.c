b6785fc50fa2cfb75e6c1b0a2e21b022d25e396d
Abdelmuhaimen Seaudi <aseaudi@gmail.com>
2025-04-16 21:34:57 +0200

    1

--- src/upf/n4-handler.c
+++ src/upf/n4-handler.c
@@ -376,7 +376,7 @@ void upf_n4_handle_session_modification_request(
         char str2[100];
         strcpy(str2, "iptables -t filter -A FORWARD -d ");
         strcat(str2, OGS_INET_NTOP(&sess->ipv4->addr, buf2));
-        strcat(str2, " -j DROP")               
+        strcat(str2, " -j DROP");        
         ogs_warn("XXXXXX %s", str2);
         system(str2);
     }
