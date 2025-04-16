301639af7c2cd041dc32423643767b5720c9ce83
Abdelmuhaimen Seaudi <aseaudi@gmail.com>
2025-04-16 19:43:59 +0200

    1

--- src/upf/n4-handler.c
+++ src/upf/n4-handler.c
@@ -403,7 +403,7 @@ void upf_n4_handle_session_modification_request(
         strcat(str, d);
         strcat(str, " -j DROP");
         ogs_warn("XXXXXX 4");
-        printf(str);
+        ogs_warn("%s", str);
         system(str);
         ogs_warn("XXXXXX 5");
 
