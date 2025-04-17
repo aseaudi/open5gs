b6fd0d5e342cb4b7fda38c031d51d53ade399b6e
Abdelmuhaimen Seaudi <aseaudi@gmail.com>
2025-04-17 19:21:16 +0200

    1

--- src/upf/n4-handler.c
+++ src/upf/n4-handler.c
@@ -401,11 +401,10 @@ exit:
     if (cause_value != OGS_PFCP_CAUSE_REQUEST_ACCEPTED)
         goto cleanup;
     for (i = 0; i < OGS_MAX_NUM_OF_URR; i++) {
-        if (ogs_pfcp_handle_update_urr(&sess->pfcp, &req->update_urr[i],
-                    &cause_value, &offending_ie_value) == NULL){
-            upf_sess_urr_acc_timers_setup(sess, req->update_urr[i]);        
-            break;
-                    }
+        ogs_pfcp_urr_t urr1 = ogs_pfcp_handle_update_urr(&sess->pfcp, &req->update_urr[i],
+            &cause_value, &offending_ie_value);
+        if (urr1) == NULL) break;
+        upf_sess_urr_acc_timers_setup(sess, urr1);
     }
     if (cause_value != OGS_PFCP_CAUSE_REQUEST_ACCEPTED)
         goto cleanup;
